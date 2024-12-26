617d97d82c0561f32963f1902235eef89aeed586
Vladislav Kovpak <123662224+engy-engy@users.noreply.github.com>
2024-12-26 18:12:58 +0700

    Add Chessboard

---
+++ src/main/java/org/example/lesson2/homework/nested_loops/Chessboard.java
@@ -0,0 +1,29 @@
+package org.example.lesson2.homework.nested_loops;
+
+import java.util.Scanner;
+
+/**
+ * App, которая выводит шахматную доску размером N x N, где клетки чередуются символами # и .
+ */
+public class Chessboard {
+
+    public static void chessboard() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите размер шахматной доски N x N");
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i +j) % 2 == 0) {
+                    System.out.print("#");
+                } else {
+                    System.out.print(".");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
