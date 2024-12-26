12f57f8a331347b77f0e44a8de75e29d3f1d259e
Vladislav Kovpak <123662224+engy-engy@users.noreply.github.com>
2024-12-26 19:46:52 +0700

    Fix Manager and Employee

--- src/main/java/org/example/lesson2/homework/constructors_oop_principles/Manager.java
+++ src/main/java/org/example/lesson2/homework/constructors_oop_principles/Manager.java
@@ -1,5 +1,17 @@
 package org.example.lesson2.homework.constructors_oop_principles;
 
+/**
+ * Наследование - Класс "Сотрудник" и "Менеджер"
+ * Создайте два класса:
+ * Employee (сотрудник) с полями:
+ * name (имя),
+ * salary (зарплата).
+ * Конструктор для инициализации полей.
+ * Метод printInfo(), выводящий информацию о сотруднике.
+ * Manager, который наследуется от Employee:
+ * Дополнительное поле bonus (премия).
+ * Переопределите метод printInfo() так, чтобы он выводил и бонус.
+ */
 public class Manager extends Employee {
 
     private double bonus;
