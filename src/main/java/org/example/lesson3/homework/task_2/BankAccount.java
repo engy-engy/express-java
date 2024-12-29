ed99ee10db5ee7818113ba7f45375248a693c174
Vladislav Kovpak <123662224+engy-engy@users.noreply.github.com>
2024-12-29 13:42:07 +0700

    Add BankAccount

---
+++ src/main/java/org/example/lesson3/homework/task_2/BankAccount.java
@@ -0,0 +1,32 @@
+package org.example.lesson3.homework.task_2;
+
+import java.util.HashSet;
+
+/**
+ * BankAccount, который будет представлять банковский счет.
+ */
+public class BankAccount {
+
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String deposit(double value) {
+        return String.format("Баланс пополнен на сумму %.2f $.%nОстаток: %.2f $", value, balance +=value);
+    }
+
+    public String withdraw(double value) {
+        if (balance < value) return String.format("На балансе не достаточно средств.%nОстаток: %.2f $", balance);
+        balance -= value;
+        return  String.format("Снято: %.2f $%nОстаток: %.2f $", value,  balance);
+    }
+
+    public String getBalance() {
+        return String.format("Текущий баланс: %.2f $", balance);
+    }
+
+}
