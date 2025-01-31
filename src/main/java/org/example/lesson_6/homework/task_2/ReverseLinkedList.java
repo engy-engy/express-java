6b600fca9cdd6a1a6a9769524bf330a14358bde1
Vladislav Kovpak <123662224+engy-engy@users.noreply.github.com>
2025-01-31 09:52:54 +0700

    Add ReverseLinkedList

--- src/main/java/org/example/lesson_6/homework/task_2/ReverseLinkedList.java
+++ src/main/java/org/example/lesson_6/homework/task_2/ReverseLinkedList.java
@@ -9,10 +9,10 @@ public class ReverseLinkedList {
         Node next;
 
         while (head != null) {
-            next = head.getNext(); // Запоминаем следующий узел
-            head.setNext(prev);    // Разворачиваем ссылку
-            prev = head;           // Двигаем prev вперёд
-            head = next;           // Двигаем head вперёд
+            next = head.getNext();
+            head.setNext(prev);
+            prev = head;
+            head = next;
         }
         return prev;
     }
@@ -31,7 +31,7 @@ public class ReverseLinkedList {
         Node n4 = new Node(4, n5);
         Node n3 = new Node(3, n4);
         Node n2 = new Node(2, n3);
-        Node n1 = new Node(1, n2);
+            Node n1 = new Node(1, n2); // head
 
         printList(n1);
         printList(reverse(n1));
