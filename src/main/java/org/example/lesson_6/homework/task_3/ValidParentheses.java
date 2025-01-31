92f063452210ff44bc8106a034f5b05f7357784c
Vladislav Kovpak <123662224+engy-engy@users.noreply.github.com>
2025-01-31 10:30:41 +0700

    Add comment ValidParentheses

--- src/main/java/org/example/lesson_6/homework/task_3/ValidParentheses.java
+++ src/main/java/org/example/lesson_6/homework/task_3/ValidParentheses.java
@@ -5,6 +5,7 @@ import java.util.Stack;
 public class ValidParentheses {
 
     public static boolean isValid(String s) {
+        if (s == null || !s.isEmpty()) return false;
 
         Stack<Character> stack = new Stack<>();
 
@@ -23,11 +24,17 @@ public class ValidParentheses {
     }
 
     public static void main(String[] args) {
-        String input1 = "()]{}";
-        String input2 = "({[(]})";
+        String input1 = "()[]{}";
+        String input2 = "()]{}";
+        String input3 = "({[(]})";
+        String input4 = "{";
+        String input5 = "";
 
         System.out.println(isValid(input1));
         System.out.println(isValid(input2));
+        System.out.println(isValid(input3));
+        System.out.println(isValid(input4));
+        System.out.println(isValid(input5));
 
     }
 }
