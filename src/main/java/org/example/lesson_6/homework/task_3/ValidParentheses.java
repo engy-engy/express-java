8626f3c4aaec68f847d11676f876b29cfd9082d8
Vladislav Kovpak <123662224+engy-engy@users.noreply.github.com>
2025-01-31 10:27:29 +0700

    Add ValidParentheses

---
+++ src/main/java/org/example/lesson_6/homework/task_3/ValidParentheses.java
@@ -0,0 +1,33 @@
+package org.example.lesson_6.homework.task_3;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+              stack.push(ch);
+            } else if (ch == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (ch == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (ch == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String input1 = "()]{}";
+        String input2 = "({[(]})";
+
+        System.out.println(isValid(input1));
+        System.out.println(isValid(input2));
+
+    }
+}
